66f25c7120ca2852ce47095fe4c0ce767ebc4196
brandon.trigueros <brandon.trigueros@ucr.ac.cr>
2023-07-05 18:16:05 -0600

    simplificacion nil node -> nullptrs, cambios itr

--- src/RBTree.hpp
+++ src/RBTree.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+#include <iostream>
 #include <string>
 
 enum Color {
@@ -8,69 +10,56 @@ enum Color {
 };
 
 template <typename Type>
-class Node {
-    private:    //ATRIBUTES
-        std::string key;
-        Type value;
-        Color color;
-        Node *left, *right, *parent;
-    public:     //CONSTRUCTORS
-        explicit Node() {               // Creates a nil node
-            this->key = "nil";
-            this->value = Type();       // TODO: default constructor for Type
-            this->color = BLACK;
-            left = right = parent = nullptr;
-        }
-        explicit Node(Type& value) {
-            this->key = value.name;
-            this->value = value;
-            this->color = RED;
-            left = right = parent = nullptr;
-        }
-    public:    //SETTERS AND GETTERS
-        void setColor(Color color) {this->color = color;}
-        void setLeft(Node<T>* left) {this->left = left;}
-        void setRight(Node<T>* right) {this->right = right;}
-        void setParent(Node<T>* parent) {this->parent = parent;}
-
-        std::string getKey() {return this->key;}
-        T& getValue() {return this->value;}
-        Color getColor() {return this->color;}
-        Node<T>* getLeft() {return this->left;}
-        Node<T>* getRight() {return this->right;}
-        Node<T>* getParent() {return this->parent;}
-};
-
-template <typename Type>
 class RBTree {
-    private:    //ATRIBUTES
-        Node<Type> *root;
-        Node<Type> *nil = new Node<Type>();
 
-    public:     //CONSTRUCTORS
+    class Node {
+        private:
+            std::string key;
+            Type value;
+            Color color;
+            Node *left, *right, *parent;
+
+        public:
+            explicit Node(Type& value, std::string key = value.name, Color color = RED, Node* left = nullptr, Node* right = nullptr, Node* parent = nullptr)
+                : key(key), value(value), color(color), left(left), right(right), parent(parent)
+            {}
+
+        public:
+            inline bool operator<(const Node& other) const {return this->key < other.key;}
+
+            void setColor(Color color) {this->color = color;}
+            void setLeft(Node* left) {this->left = left;}
+            void setRight(Node* right) {this->right = right;}
+
+            void setParent(Node* parent) {this->parent = parent;}
+            std::string getKey() {return this->key;}
+            Type& getValue() {return this->value;}
+            Color getColor() {return this->color;}
+            Node* getLeft() {return this->left;}
+            Node* getRight() {return this->right;}
+            Node* getParent() {return this->parent;}
+    };
+
+    private:
+        Node* root;
+
+    public:
         RBTree () {
-            this->root = this->nil;
+            this->root = nullptr;
         }
-        explicit RBTree (Type& value) {
-            this->root = new Node<Type>(value);
-            this->root->setColor(BLACK);
-            this->root->setLeft(this->nil);
-            this->root->setRight(this->nil);
-        }
-
-    public:     //METHODS
-        void insertNode(Node<Type>* node){
-            if (this->root == this->nil) {
+        
+        void insertNode(Node* node) {
+            if (!this->root) {
                 this->root = node;
                 this->root->setColor(BLACK);
-                this->root->setLeft(this->nil);
-                this->root->setRight(this->nil);
+                this->root->setLeft(nullptr);
+                this->root->setRight(nullptr);
             }
             else {
-                Node<Type> current = this->root;
-                while (current != this->nil) {
-                    if (node->getKey < current.getKey()) {
-                        if (current.getLeft() == this->nil) {
+                Node* current = this->root;
+                while (current) {
+                    if (*node < *current) {
+                        if (!current.getLeft()) {
                             current.setLeft(node);
                             node->setParent(current);
                             break;
@@ -80,7 +69,7 @@ class RBTree {
                         }
                     }
                     else {
-                        if (current.getRight() == this->nil) {
+                        if (!current.getRight()) {
                             current.setRight(node);
                             node->setParent(current);
                             break;
@@ -92,41 +81,119 @@ class RBTree {
                 }
             }
             node->setColor(RED);
-            node->setLeft(this->nil);
-            node->setRight(this->nil);
+            node->setLeft(nullptr);
+            node->setRight(nullptr);
             this->fixup (node);
         }
 
         void insertNode(Type& value){
-            Node<Type> node = new Node<Type>(value);
+            Node* node = new Node(value);
             this->insertNode(node);
         }
 
-        Node<T>* search(Node<T> *node, std::string key){
+    //     Node* search(Node* node, std::string key){
 
+    //     }
+
+    // private:
+    //     void leftRotate(Node* node){
+
+    //     }
+
+    //     void rightRotate(Node* node){
+
+    //     }
+
+    //     void fixup(Node* node){
+
+    //     }
+
+    //     void clear(Node* node){
+
+    //     }
+
+        static Node *findMinimum(Node *subtree) {
+            if (subtree) {
+                while (subtree->left) {
+                    subtree = subtree->left;
+                }
+            }
+            return subtree;
         }
 
-    private:    //FIX UP METHODS
-        void leftRotate(Node<T>* node){
+        static Node& findNextNodeOrder (Node* current) {
+            Node* original = current;
+            if (current->right) {
+                return RNTree::findMinimum(node->right);
+            }
+            while (current->parent && current == current->parent->right) {
+                current = node->parent;
+                if (!current->parent) {
+                    return = nullptr;
+                }
+            }
+            if (current && current->parent && current == current->parent->left)
+                current = current->parent;
 
+            return current == original ? nullptr : current;
         }
 
-        void rightRotate(Node<T>* node){
+        class Iterator {
+            private:
+                Node* node;
 
+            public:
+                explicit Iterator(Node* node)
+                    : node(node)
+                {}
+                
+                DECLARE_RULE5(Iterator, default);
+
+                inline bool operator != (const Iterator& other) const {
+                    return this->node != other.node;
+                }
+
+                inline std::string getKey() {
+                    assert(this->node);
+                    return this->node->key;
+                }
+
+                inline const std::string getKey() const {
+                    assert(this->node);
+                    return this->node->key;
+                }
+
+                inline Type& getValue()  {
+                    assert(this->node);
+                    return this->node->value;
+                }
+
+                inline const Type& getValue() const {
+                    assert(this->node);
+                    return this->node->value;
+                }
+
+                inline Iterator& operator ++() {
+                    this->node = RBTree::findNextNodeOrder(this->node);
+                    return *this;
+                }
+
+        };
+
+        Iterator begin() {
+                return Iterator(this->findMinimum(this->root));
         }
 
-        void fixup(Node<T>* node){
-
+        Iterator end(){
+                return Iterator(nullptr);
         }
 
-        void clear(Node<T>* node){
-
-        }
-
-    public:     //RULE OF FIVE
+    public:
 
         RBTree(const RBTree& other) {           //copy constructor
-
+            for (Iterator it = other.begin(); it != other.end(); ++it) {
+                this->insertNode(it.getValue());
+            }
         }
 
         RBTree(RBTree&& other){                 //move constructor
